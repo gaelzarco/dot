d2b1a9420283a16655bb79c711bb4ff53c6061c4
Gael <gaelxarco@icloud.com>
2024-10-31 15:39:43 -0700

    updates

--- hypr/scripts/hyprlock.sh
+++ hypr/scripts/hyprlock.sh
@@ -6,7 +6,7 @@ grim -s 1.5 -l 0 ~/.cache/screenlock.png
 
 # Scale image resolution down for performance, apply blur, resize, and save 
 # Be sure to set hyprlock to look for wallpaper in ~/.cache/screenlock.png
-magick ~/.cache/screenlock.png -scale 20% -blur 0x2 -modulate 30,100,100 -resize 200% ~/.cache/screenlock.png
+magick ~/.cache/screenlock.png -scale 20% -blur 0x1 -modulate 50,100,100 -resize 200% ~/.cache/screenlock.png
 
 # Call hyprlock after the 
 hyprlock

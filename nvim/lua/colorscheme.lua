c8b13038e39f31b5e66ca395f7669a869ec26e3c
Gael <gaelxarco@icloud.com>
2025-03-14 13:46:34 -0700

    dark metal

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,7 +1,7 @@
-local colorscheme = 'base16-black-metal-immortal'
+local colorscheme = 'base16-black-metal-dark-funeral'
 
 -- vim.opt.background = "dark" -- set this to dark or light
--- 
+
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
@@ -11,3 +11,9 @@ if not is_ok then
     vim.notify('colorscheme ' .. colorscheme .. ' not found!')
     return
 end
+
+-- Uncomment the lines below to prevent overwriting transparent bg
+-- vim.cmd("autocmd ColorScheme * highlight Normal guibg=NONE ctermbg=NONE")
+
+-- vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+-- vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")

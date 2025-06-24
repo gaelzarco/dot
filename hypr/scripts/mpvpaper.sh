665176756b4c5d41484e377b15f9889435f86089
Gael <gaelxarco@icloud.com>
2025-06-23 23:42:02 -0700

    changes

--- hypr/scripts/mpvpaper.sh
+++ hypr/scripts/mpvpaper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Dependencies: mpvpaper 
+# Ensure to change DP-4 to proper monitor output
 
-mpvpaper -o "--input-ipc-server=/tmp/mpvsocket no-audio --loop-file=inf" DP-1 "$HYPR_WALLVIDEO"
+mpvpaper -o "--input-ipc-server=/tmp/mpvsocket no-audio --loop-file=inf" DP-4 "$HYPR_WALLVIDEO"

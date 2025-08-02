49fcd51249a3bb7e89c29f0a7f1e3c84b1e09455
Gael Zarco <gaelxarco@icloud.com>
2025-08-01 23:04:54 -0700

    simplified neovim config; nightly version

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,96 @@
-require('options')
-require('plugins')
-require('lsp')
-require('keymaps')
-require('colorscheme')
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.termguicolors = true
+vim.o.wrap = false
+vim.o.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.o.swapfile = false
+vim.o.fillchars = 'eob: '
+vim.o.textwidth = 80
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+vim.o.clipboard = "unnamedplus"
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+
+vim.pack.add({
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/echasnovski/mini.pick" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/windwp/nvim-autopairs" },
+  { src = "https://github.com/windwp/nvim-ts-autotag" },
+  { src = "https://github.com/lervag/vimtex" },
+  { src = "https://github.com/metalelf0/base16-black-metal-scheme" }
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+vim.cmd("set completeopt+=noselect")
+
+require("mini.pick").setup()
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "typescript",
+    "javascript",
+    "rust",
+    "python",
+    "c",
+    "cpp",
+    "lua",
+    "bash",
+    "html",
+    "css"
+  },
+  highlight = { enable = true }
+})
+require("oil").setup()
+require("nvim-ts-autotag").setup()
+
+vim.g.vimtex_view_method = "zathura"
+vim.keymap.set("n", "U", "<C-r>", { desc = "Redo last change" })
+vim.keymap.set("n", "<leader>R", [[:%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Replace word under cursor" })
+vim.keymap.set("n", "<leader>n", ":bn<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>b", ":bp<CR>", { desc = "Previous buffer" })
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover)
+vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition)
+vim.keymap.set('n', '<leader>D', vim.lsp.buf.declaration)
+vim.keymap.set('n', '<leader>i', vim.lsp.buf.implementation)
+vim.keymap.set('n', '<leader>o', vim.lsp.buf.type_definition)
+vim.keymap.set('n', '<leader>r', vim.lsp.buf.references)
+vim.keymap.set('n', '<leader>s', vim.lsp.buf.signature_help)
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.lsp.enable({
+  "lua_ls",
+  "tsserver",
+  "rust_analyzer",
+  "pyright",
+  "clangd",
+  "bashls",
+  "html",
+  "css"
+})
+
+vim.cmd("colorscheme base16-black-metal-immortal")
+vim.cmd(":hi statusline guibg=NONE")
+vim.cmd(":hi LineNr guibg=NONE")
+vim.cmd(":hi CursorLineNr guibg=NONE")
+vim.cmd(":hi SignColumn guibg=NONE")

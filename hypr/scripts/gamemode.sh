aafa6a2e1fd458e85263fb95eb00ac561daa7222
Gael <gaelxarco@icloud.com>
2025-10-06 17:35:04 -0700

    changes

--- hypr/scripts/gamemode.sh
+++ hypr/scripts/gamemode.sh
@@ -3,10 +3,12 @@ HYPRGAMEMODE=$(hyprctl getoption decoration:blur:enabled | awk 'NR==1{print $2}'
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
-        keyword animation borderangle,0; \
+        keyword animation borderangle,0;\
         keyword decoration:shadow:enabled 0;\
         keyword decoration:blur:enabled 0;\
         keyword decoration:fullscreen_opacity 1;\
+        keyword decoration:active_opacity 1;\
+        keyword decoration:inactive_opacity 1;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\

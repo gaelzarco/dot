564c07319debce20b8b867539418d83f78496cc6
Gael <gaelxarco@icloud.com>
2025-10-22 12:13:47 -0700

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,22 @@
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.o.termguicolors = true
+vim.o.wildmenu = true
 vim.o.wrap = false
+vim.o.showmatch = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.opt.colorcolumn = "80"
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+vim.o.list = true
+vim.opt.listchars = { tab = "→ ", trail = "·", nbsp = "␣" }
 vim.o.expandtab = true
 vim.o.swapfile = false
-vim.o.fillchars = 'eob: '
+vim.o.pumheight = 10
+vim.o.signcolumn = "yes"
+vim.o.termguicolors = true
+vim.o.fillchars = "eob: "
 vim.o.textwidth = 80
 vim.o.winborder = "rounded"
 vim.o.clipboard = "unnamedplus"
